b081562923faa1f941e01df1e9efab4409edc871
root <root@localhost.localdomain>
2026-03-16 09:55:16 +0800

    平衡缘分系统：加成降至5%，增加特殊效果触发

--- src/main/java/com/game/service/FateService.java
+++ src/main/java/com/game/service/FateService.java
@@ -6,20 +6,18 @@ import org.springframework.stereotype.Service;
 import java.util.*;
 
 /**
- * 缘分计算服务
+ * 缘分计算服务 v2
+ * 平衡调整
  */
 @Service
 public class FateService {
     
     /**
      * 计算队伍缘分加成
-     * @param partners 队伍中的伙伴列表
-     * @return 缘分加成信息
      */
     public Map<String, Object> calculateFateBonus(List<Partner> partners) {
         Map<String, Object> result = new HashMap<>();
         
-        // 获取伙伴名称列表
         List<String> heroNames = new ArrayList<>();
         for (Partner p : partners) {
             if (p.getName() != null) {
@@ -27,13 +25,20 @@ public class FateService {
             }
         }
         
-        // 检查所有激活的缘分
         List<Map<String, Object>> activeFates = new ArrayList<>();
         int totalAtkBonus = 0;
         int totalHpBonus = 0;
         int totalDefBonus = 0;
         int totalCritBonus = 0;
         int totalDamageBonus = 0;
+        
+        // 特殊效果
+        int skillTriggerBonus = 0;
+        int extraDamageBonus = 0;
+        int damageReduceBonus = 0;
+        int healBonus = 0;
+        int critBoostBonus = 0;
+        int speedBonus = 0;
         
         for (Map<String, Object> fate : FateConfig.FATES) {
             @SuppressWarnings("unchecked")
@@ -64,9 +69,7 @@ public class FateService {
                 activated.put("type", type.name());
                 activated.put("value", value);
                 
-                activeFates.add(activated);
-                
-                // 累加属性
+                // 基础属性加成
                 switch (type) {
                     case ATK: totalAtkBonus += value; break;
                     case HP: totalHpBonus += value; break;
@@ -74,6 +77,24 @@ public class FateService {
                     case CRIT: totalCritBonus += value; break;
                     case DAMAGE: totalDamageBonus += value; break;
                 }
+                
+                // 特殊效果加成
+                FateConfig.FateEffect effect = (FateConfig.FateEffect) fate.get("effect");
+                int effectValue = (Integer) fate.get("effectValue");
+                activated.put("effect", effect.name());
+                activated.put("effectValue", effectValue);
+                activated.put("effectDesc", FateConfig.getEffectDesc(effect, effectValue));
+                
+                switch (effect) {
+                    case SKILL_TRIGGER: skillTriggerBonus += effectValue; break;
+                    case EXTRA_DAMAGE: extraDamageBonus += effectValue; break;
+                    case DAMAGE_REDUCE: damageReduceBonus += effectValue; break;
+                    case HEAL: healBonus += effectValue; break;
+                    case CRIT_BOOST: critBoostBonus += effectValue; break;
+                    case SPEED_BOOST: speedBonus += effectValue; break;
+                }
+                
+                activeFates.add(activated);
             }
         }
         
@@ -83,6 +104,12 @@ public class FateService {
         result.put("totalDefBonus", totalDefBonus);
         result.put("totalCritBonus", totalCritBonus);
         result.put("totalDamageBonus", totalDamageBonus);
+        result.put("skillTriggerBonus", skillTriggerBonus);
+        result.put("extraDamageBonus", extraDamageBonus);
+        result.put("damageReduceBonus", damageReduceBonus);
+        result.put("healBonus", healBonus);
+        result.put("critBoostBonus", critBoostBonus);
+        result.put("speedBonus", speedBonus);
         
         return result;
     }
@@ -97,6 +124,9 @@ public class FateService {
             @SuppressWarnings("unchecked")
             List<String> heroes = (List<String>) f.get("heroes");
             f.put("heroesCount", heroes.size());
+            // 添加效果描述
+            FateConfig.FateEffect effect = (FateConfig.FateEffect) f.get("effect");
+            f.put("effectDesc", FateConfig.getEffectDesc(effect, (Integer)f.get("effectValue")));
             fates.add(f);
         }
         return fates;
@@ -120,6 +150,11 @@ public class FateService {
                 f.put("icon", fate.get("icon"));
                 f.put("type", ((FateConfig.FateType)fate.get("type")).name());
                 f.put("value", fate.get("value"));
+                f.put("effect", ((FateConfig.FateEffect)fate.get("effect")).name());
+                f.put("effectValue", fate.get("effectValue"));
+                f.put("effectDesc", FateConfig.getEffectDesc(
+                    (FateConfig.FateEffect)fate.get("effect"), 
+                    (Integer)fate.get("effectValue")));
                 f.put("required", required);
                 f.put("requiredCount", required.size());
                 result.add(f);
